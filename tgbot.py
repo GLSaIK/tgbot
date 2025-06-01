8e4fb1a47358a2cd8509b6a3c279fe6238619b68
GLSaIK <164785737+GLSaIK@users.noreply.github.com>
2025-06-01 15:06:52 +0300

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,5 +1,4 @@
 import nest_asyncio
-import os
 nest_asyncio.apply()
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -37,7 +36,7 @@ async def startcom(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # Основная функция запуска бота
 async def main():
     # Замени 'YOUR_TOKEN_HERE' на токен, выданный BotFather
-    app = ApplicationBuilder().token(os.getenv(BOT_TOKEN)).build()
+    app = ApplicationBuilder().token("7900235290:AAESSXMl2oaGZt1KpsodehxFAlb9ktdonaw").build()
 
     # Добавляем команду
     app.add_handler(CommandHandler("start", startcom))
